fd3cd9ce1340a60bb480436c13044df64c4c625f
Trung <ductrung4263@gmail.com>
2021-11-29 11:12:17 +0700

    change text 3

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React deployment
+          Learn React deployment Wih Netlify
         </a>
       </header>
     </div>
